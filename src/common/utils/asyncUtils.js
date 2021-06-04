71d8f1070c2964640206ec0b32b67d0b1e57586d
lwd3737 <lwd3737@naver.com>
2021-06-04 09:29:32 +0900

    up to date

--- src/common/utils/asyncUtils.js
+++ src/common/utils/asyncUtils.js
@@ -1,17 +1,17 @@
 export const createAsyncThunk = (type, asyncApi) => {
-    return (param) => async (dispatch, getState) => {
-        const [SUCCESS, FAILED] = [`${type}_SUCCESS`, `${type}_FAILED`];
-    
-        dispatch({
-            type, 
-            param
-        });
+  return (param) => async (dispatch, getState) => {
+    const [SUCCESS, FAILED] = [`${type}_SUCCESS`, `${type}_FAILED`];
 
-        try{
-            const payload = await asyncApi(param);
-            dispatch({ type: SUCCESS, payload});
-        } catch(e){
-            dispatch({ type: FAILED, error: e});
-        }
+    dispatch({
+      type,
+      param,
+    });
+
+    try {
+      const payload = await asyncApi(param);
+      dispatch({ type: SUCCESS, payload });
+    } catch (e) {
+      dispatch({ type: FAILED, error: e });
     }
+  };
 };
